4888dd65529ec5ad40f49818119e18ca661b400c
julimancan <julian.m.bustos@gmail.com>
2021-02-04 12:21:49 -0500

    added the menu for the red box

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -4,3 +4,9 @@ const onClickMenu = () => {
   document.getElementById("menu-bg").classList.toggle("change-bg");
   
 }
+const onClickMenuRed = () => {
+  document.getElementById("menu-red").classList.toggle("change-red");
+  document.getElementById("nav-red").classList.toggle("change-red");
+  document.getElementById("menu-bg-red").classList.toggle("change-bg-red");
+  
+}
