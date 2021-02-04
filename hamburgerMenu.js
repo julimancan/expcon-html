8d8970baef7cd575a462eab524f8e989689aebca
julimancan <julian.m.bustos@gmail.com>
2021-02-04 11:19:28 -0500

    added background and list items to menu with animations

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,3 +1,6 @@
 const onClickMenu = () => {
-  document.getElementById("menu").classList.toggle("change")
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+  
 }
