5bcbe555102545f13330fd76d32778fb17227ba9
julimancan <julian.m.bustos@gmail.com>
2021-02-04 13:03:14 -0500

    added the yellow menu with animations

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -12,6 +12,13 @@ const onClickMenuRed = () => {
   document.getElementById("bar2-red").classList.toggle("change-red");
   document.getElementById("bar3-red").classList.toggle("change-red");
   document.getElementById("nav-title-red").classList.toggle("change-nav-title-red");
-
-  
+}
+const onClickMenuYellow = () => {
+  document.getElementById("menu-yellow").classList.toggle("change-yellow");
+  document.getElementById("nav-yellow").classList.toggle("change-yellow");
+  document.getElementById("menu-bg-yellow").classList.toggle("change-bg-yellow");
+  document.getElementById("bar1-yellow").classList.toggle("change-yellow");
+  document.getElementById("bar2-yellow").classList.toggle("change-yellow");
+  document.getElementById("bar3-yellow").classList.toggle("change-yellow");
+  document.getElementById("nav-title-yellow").classList.toggle("change-nav-title-yellow");
 }
