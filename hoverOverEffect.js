4e31625e45f2197636b7cc8f4f18f3cc58c6afa2
julimancan <julian.m.bustos@gmail.com>
2021-02-05 18:01:47 -0500

    styled the mouse better

--- hoverOverEffect.js
+++ hoverOverEffect.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $(".mouse_hover_area").mousemove(function(event) {
     var top = $(window).scrollTop() - $(this).offset().top + event.clientY;
     var left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
-    $(".mouse").css({"left":left - 150, "top":top -150});
+    $(".mouse").css({"left":left - 100, "top":top - 100});
   });
 });
