f39f43fe0a589df8fb31abd76b429ebe14ed2273
julimancan <julian.m.bustos@gmail.com>
2021-02-05 17:07:48 -0500

    added the images to the circles

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,9 +1,9 @@
+
 const onClickMenu = () => {
   document.getElementById("menu").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
-  
-}
+};
 const onClickMenuRed = () => {
   document.getElementById("menu-red").classList.toggle("change-red");
   document.getElementById("nav-red").classList.toggle("change-red");
@@ -11,17 +11,23 @@ const onClickMenuRed = () => {
   document.getElementById("bar1-red").classList.toggle("change-red");
   document.getElementById("bar2-red").classList.toggle("change-red");
   document.getElementById("bar3-red").classList.toggle("change-red");
-  document.getElementById("nav-title-red").classList.toggle("change-nav-title-red");
-}
+  document
+    .getElementById("nav-title-red")
+    .classList.toggle("change-nav-title-red");
+};
 const onClickMenuYellow = () => {
   document.getElementById("menu-yellow").classList.toggle("change-yellow");
   document.getElementById("nav-yellow").classList.toggle("change-yellow");
-  document.getElementById("menu-bg-yellow").classList.toggle("change-bg-yellow");
+  document
+    .getElementById("menu-bg-yellow")
+    .classList.toggle("change-bg-yellow");
   document.getElementById("bar1-yellow").classList.toggle("change-yellow");
   document.getElementById("bar2-yellow").classList.toggle("change-yellow");
   document.getElementById("bar3-yellow").classList.toggle("change-yellow");
-  document.getElementById("nav-title-yellow").classList.toggle("change-nav-title-yellow");
-}
+  document
+    .getElementById("nav-title-yellow")
+    .classList.toggle("change-nav-title-yellow");
+};
 const onClickMenuBlack = () => {
   document.getElementById("menu-black").classList.toggle("change-black");
   document.getElementById("nav-black").classList.toggle("change-black");
@@ -29,23 +35,33 @@ const onClickMenuBlack = () => {
   document.getElementById("bar1-black").classList.toggle("change-black");
   document.getElementById("bar2-black").classList.toggle("change-black");
   document.getElementById("bar3-black").classList.toggle("change-black");
-  document.getElementById("nav-title-black").classList.toggle("change-nav-title-black");
-}
+  document
+    .getElementById("nav-title-black")
+    .classList.toggle("change-nav-title-black");
+};
 const onClickMenuBlack2 = () => {
   document.getElementById("menu-black2").classList.toggle("change-black2");
   document.getElementById("nav-black2").classList.toggle("change-black2");
-  document.getElementById("menu-bg-black2").classList.toggle("change-bg-black2");
+  document
+    .getElementById("menu-bg-black2")
+    .classList.toggle("change-bg-black2");
   document.getElementById("bar1-black2").classList.toggle("change-black2");
   document.getElementById("bar2-black2").classList.toggle("change-black2");
   document.getElementById("bar3-black2").classList.toggle("change-black2");
-  document.getElementById("nav-title-black2").classList.toggle("change-nav-title-black2");
-}
+  document
+    .getElementById("nav-title-black2")
+    .classList.toggle("change-nav-title-black2");
+};
 const onClickMenuSkyblue = () => {
   document.getElementById("menu-skyblue").classList.toggle("change-skyblue");
   document.getElementById("nav-skyblue").classList.toggle("change-skyblue");
-  document.getElementById("menu-bg-skyblue").classList.toggle("change-bg-skyblue");
+  document
+    .getElementById("menu-bg-skyblue")
+    .classList.toggle("change-bg-skyblue");
   document.getElementById("bar1-skyblue").classList.toggle("change-skyblue");
   document.getElementById("bar2-skyblue").classList.toggle("change-skyblue");
   document.getElementById("bar3-skyblue").classList.toggle("change-skyblue");
-  document.getElementById("nav-title-skyblue").classList.toggle("change-nav-title-skyblue");
-}
+  document
+    .getElementById("nav-title-skyblue")
+    .classList.toggle("change-nav-title-skyblue");
+};
