8ef0a61b7aa9c3215d4dea65cac6118191acd762
julimancan <julian.m.bustos@gmail.com>
2021-02-05 17:55:48 -0500

    added working hover over effect

--- hoverOverEffect.js
+++ hoverOverEffect.js
@@ -1,7 +1,7 @@
-$(document).ready(() => {
-  $(".mouse_hover_area").mousemove((event) => {
-    var top = $(window).scrollTop() - $(this).offset().top + event.clientY
-    var left = $(window).scrollLeft() - $(this).offset().left + event.clientX
-    $(".mouse").css({"left":left - 150, "top":top -150})
+$(document).ready(function() {
+  $(".mouse_hover_area").mousemove(function(event) {
+    var top = $(window).scrollTop() - $(this).offset().top + event.clientY;
+    var left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
+    $(".mouse").css({"left":left - 150, "top":top -150});
   });
 });
