aa9834f000a092a64755f341b6c215bd592cb17d
julimancan <julian.m.bustos@gmail.com>
2021-02-06 15:33:56 -0500

    added the payments page with styling

--- audio.js
+++ audio.js
@@ -1,62 +1,12 @@
-// const AudioContext = window.AudioContext || window.webkitAudioContext;
-// const audioCtx = new AudioContext();
-
-// const audioElement = document.querySelector('audio');
-// const track = audioCtx.createMediaElementSource(audioElement);
-
-// const playButton = document.querySelector('.tape-controls-play');
-
-// // play pause audio
-// playButton.addEventListener('click', function() {
-	
-// 	// check if context is in suspended state (autoplay policy)
-// 	if (audioCtx.state === 'suspended') {
-// 		audioCtx.resume();
-// 	}
-	
-// 	if (this.dataset.playing === 'false') {
-// 		audioElement.play();
-// 		this.dataset.playing = 'true';
-// 	// if track is playing pause it
-// 	} else if (this.dataset.playing === 'true') {
-// 		audioElement.pause();
-// 		this.dataset.playing = 'false';
-// 	}
-	
-// 	let state = this.getAttribute('aria-checked') === "true" ? true : false;
-// 	this.setAttribute( 'aria-checked', state ? "false" : "true" );
-	
-// }, false);
-
-// // if track ends
-// audioElement.addEventListener('ended', () => {
-// 	playButton.dataset.playing = 'false';
-// 	playButton.setAttribute( "aria-checked", "false" );
-// }, false);
-
-
-const x = document.getElementById("audio");
-
-function playSong() {
-	x.play();
-};
-
-function pauseSong(){
-	x.pause();
-};
+const audio = document.getElementById("audio");
 
 let isPlaying = false;
-function playMusic() {
-	console.log(isPlaying)
-	if (isPlaying) {
-		pauseSong();
+const playMusic = () => {
+  if (!isPlaying) {
+    audio.play();
+    isPlaying = true;
+  } else {
 		isPlaying = false;
-		console.log(isPlaying, "set to pause");
-} else {
-		if (!isPlaying) {
-			isPlaying = true;
-			playSong();
-			console.log(isPlaying, "set to play");
-		}
+		audio.pause();
 	}
-}
+};
--- hoverOverEffect.js
+++ hoverOverEffect.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
-  $(".mouse_hover_area").mousemove(function(event) {
+$(document).ready(function () {
+  $(".mouse_hover_area").mousemove(function (event) {
     var top = $(window).scrollTop() - $(this).offset().top + event.clientY;
     var left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
-    $(".mouse").css({"left":left - 100, "top":top - 100});
+    $(".mouse").css({ left: left - 100, top: top - 100 });
+  });
+  $(".mouse_hover_area_red").mousemove(function (event) {
+    var top = $(window).scrollTop() - $(this).offset().top + event.clientY;
+    var left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
+    $(".mouse-red").css({ left: left - 100, top: top - 100 });
   });
 });
