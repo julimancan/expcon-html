5ecb7c35ba319ee727a63447551d09d40cabe257
julimancan <julian.m.bustos@gmail.com>
2021-02-06 17:30:06 -0500

    added the click circle as a hover over the speakers effect and fixed basic styling

--- hoverOverEffect.js
+++ hoverOverEffect.js
@@ -7,6 +7,6 @@ $(document).ready(function () {
   $(".mouse_hover_area_red").mousemove(function (event) {
     var top = $(window).scrollTop() - $(this).offset().top + event.clientY;
     var left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
-    $(".mouse-red").css({ left: left - 100, top: top - 100 });
+    $(".mouse_red").css({ left: left + 200, top: top + 140 });
   });
 });
