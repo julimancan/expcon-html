b4fa6a0e299b0cfe5ec08e5ca6ee63fd22a349c3
julimancan <julian.m.bustos@gmail.com>
2021-02-07 15:31:41 -0500

    added the disabled button to the payments page and functionality

--- payments.js
+++ payments.js
@@ -3,3 +3,27 @@ window.onload = () => {
   console.log(plan);
   document.getElementById(`select-${plan}`).click();
 };
+
+function checkform(form) {
+  // get all the inputs within the submitted form
+  const inputs = form.getElementsByClassName('input-class');
+
+  // inputs.forEach(input => {
+    console.log(inputs[0].value.length)
+
+    for (let input of inputs) {
+      console.log(input.value)
+      if(input.hasAttribute("required")) {
+        if (input.value.length > 0) {
+          console.log("im here with a long value");
+          $("#on-pricing").css({ display: "block" });
+          $("#disabled").css({ display: "none" });
+          return 
+        } else if (input.value.length === 0) {
+            $("#on-pricing").css({ display: "none" });
+            $("#disabled").css({ display: "block" });
+          }
+        }
+      }
+    }
+    
