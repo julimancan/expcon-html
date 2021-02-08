ac5f01f676b90aa5bc7a6ea928bdb1335bbaeef7
julimancan <julian.m.bustos@gmail.com>
2021-02-07 23:26:03 -0500

    fixed the styling for all the pages

--- hoverOverEffect.js
+++ hoverOverEffect.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
   $(".mouse_hover_area").mousemove(function (event) {
-    var top = $(window).scrollTop() - $(this).offset().top + event.clientY;
-    var left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
+    let top = $(window).scrollTop() - $(this).offset().top + event.clientY;
+    let left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
     $(".mouse").css({ left: left - 100, top: top - 100 });
   });
 
   $(".mouse_hover_area_red_right").mousemove(function (event) {
-    var top = $(window).scrollTop() - $(this).offset().top + event.clientY;
-    var left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
+    let top = $(window).scrollTop() - $(this).offset().top + event.clientY;
+    let left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
     $(".mouse_red_right").css({ left: left + 400, top: top + 190 });
   });
   $(".mouse_hover_area_red_left").mousemove(function (event) {
-    var top = $(window).scrollTop() - $(this).offset().top + event.clientY;
-    var left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
+    let top = $(window).scrollTop() - $(this).offset().top + event.clientY;
+    let left = $(window).scrollLeft() - $(this).offset().left + event.clientX;
     $(".mouse_red_left").css({ left: left + 0, top: top + 190 });
   });
 });
