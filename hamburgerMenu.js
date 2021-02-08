4ff6e41344581dfdb31d3e5a6250d76fe0d1c69f
julimancan <julian.m.bustos@gmail.com>
2021-02-07 19:28:36 -0500

    refactored the file hamburgerMenu.js

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,79 +1,10 @@
-const onClickMenu = () => {
-  document.getElementById("menu").classList.toggle("change");
-  document.getElementById("nav").classList.toggle("change");
-  document.getElementById("menu-bg").classList.toggle("change-bg");
-};
-const onClickMenuRed = () => {
-  document.getElementById("menu-red").classList.toggle("change-red");
-  document.getElementById("nav-red").classList.toggle("change-red");
-  document.getElementById("menu-bg-red").classList.toggle("change-bg-red");
-  document.getElementById("bar1-red").classList.toggle("change-red");
-  document.getElementById("bar2-red").classList.toggle("change-red");
-  document.getElementById("bar3-red").classList.toggle("change-red");
-  document
-    .getElementById("nav-title-red")
-    .classList.toggle("change-nav-title-red");
-};
-const onClickMenuYellow = () => {
-  document.getElementById("menu-yellow").classList.toggle("change-yellow");
-  document.getElementById("nav-yellow").classList.toggle("change-yellow");
-  document
-    .getElementById("menu-bg-yellow")
-    .classList.toggle("change-bg-yellow");
-  document.getElementById("bar1-yellow").classList.toggle("change-yellow");
-  document.getElementById("bar2-yellow").classList.toggle("change-yellow");
-  document.getElementById("bar3-yellow").classList.toggle("change-yellow");
-  document
-    .getElementById("nav-title-yellow")
-    .classList.toggle("change-nav-title-yellow");
-};
-const onClickMenuBlack = () => {
-  document.getElementById("menu-black").classList.toggle("change-black");
-  document.getElementById("nav-black").classList.toggle("change-black");
-  document.getElementById("menu-bg-black").classList.toggle("change-bg-black");
-  document.getElementById("bar1-black").classList.toggle("change-black");
-  document.getElementById("bar2-black").classList.toggle("change-black");
-  document.getElementById("bar3-black").classList.toggle("change-black");
-  document
-    .getElementById("nav-title-black")
-    .classList.toggle("change-nav-title-black");
-};
-const onClickMenuBlack2 = () => {
-  document.getElementById("menu-black2").classList.toggle("change-black2");
-  document.getElementById("nav-black2").classList.toggle("change-black2");
-  document
-    .getElementById("menu-bg-black2")
-    .classList.toggle("change-bg-black2");
-  document.getElementById("bar1-black2").classList.toggle("change-black2");
-  document.getElementById("bar2-black2").classList.toggle("change-black2");
-  document.getElementById("bar3-black2").classList.toggle("change-black2");
-  document
-    .getElementById("nav-title-black2")
-    .classList.toggle("change-nav-title-black2");
-};
-const onClickMenuSkyblue = () => {
-  document.getElementById("menu-skyblue").classList.toggle("change-skyblue");
-  document.getElementById("nav-skyblue").classList.toggle("change-skyblue");
-  document
-    .getElementById("menu-bg-skyblue")
-    .classList.toggle("change-bg-skyblue");
-  document.getElementById("bar1-skyblue").classList.toggle("change-skyblue");
-  document.getElementById("bar2-skyblue").classList.toggle("change-skyblue");
-  document.getElementById("bar3-skyblue").classList.toggle("change-skyblue");
-  document
-    .getElementById("nav-title-skyblue")
-    .classList.toggle("change-nav-title-skyblue");
-};
-const onClickMenuWhite = () => {
-  document.getElementById("menu-white").classList.toggle("change-white");
-  document.getElementById("nav-white").classList.toggle("change-white");
-  document
-    .getElementById("menu-bg-white")
-    .classList.toggle("change-bg-white");
-  document.getElementById("bar1-white").classList.toggle("change-white");
-  document.getElementById("bar2-white").classList.toggle("change-white");
-  document.getElementById("bar3-white").classList.toggle("change-white");
-  document
-    .getElementById("nav-title-white")
-    .classList.toggle("change-nav-title-white");
+
+const onClickMenu = (menu) => {
+  document.getElementById(`menu-${menu}`).classList.toggle(`change-${menu}`);
+  document.getElementById(`nav-${menu}`).classList.toggle(`change-${menu}`);
+  document.getElementById(`menu-bg-${menu}`).classList.toggle(`change-bg-${menu}`);
+  document.getElementById(`bar1-${menu}`).classList.toggle(`change-${menu}`);
+  document.getElementById(`bar2-${menu}`).classList.toggle(`change-${menu}`);
+  document.getElementById(`bar3-${menu}`).classList.toggle(`change-${menu}`);
+  document.getElementById(`nav-title-${menu}`).classList.toggle(`change-nav-title-${menu}`); 
 };
